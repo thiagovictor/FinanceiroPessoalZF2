d4f0c46c8d7536395ed015feec8f9ab99de8526b
Thiago Victor <thiago_vick@yahoo.com.br>
2014-05-24 00:21:10 -0300

    formularios

---
+++ module/CentroCusto/src/CentroCusto/Form/CentroCustoForm.php
@@ -0,0 +1,43 @@
+<?php
+
+
+namespace CentroCusto\Form;
+
+use Zend\Form\Form;
+
+class CentroCustoForm extends Form{
+   
+    public function __construct($name = null) {
+        parent::__construct($name);
+        $this->setAttribute('method', 'post');
+        $this->setInputFilter(new CentroCustoFilter);
+        
+        $this->add(array(
+           'name' =>'id',
+            'attibutes' => array(
+                'type' => 'hidden'
+            )
+        ));
+        
+        $this->add(array(
+           'name' => 'nome',
+            'options' => array(
+                'type' => 'text',
+                'label' => 'Nome'
+            ),
+            'attributes' => array(
+                'id' => 'nome',
+                'placeholder' => 'Entre com o nome'
+            )
+        ));
+        
+        $this->add(array(
+           'name' => 'submit',
+            'type' => 'Zend\Form\Element\Submit',
+            'attributes' => array(
+                'value' => 'Salvar',
+                'class' => 'btn-success'
+            )
+        ));
+    }
+}
